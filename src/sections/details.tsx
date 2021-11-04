08dc74a008fdf6b0dbe835c41c72c4eebbf3379f
Elliot Hershberg <eahershberg@gmail.com>
2021-11-04 16:11:46 -0700

    Use custom theme

--- src/sections/details.tsx
+++ src/sections/details.tsx
@@ -59,7 +59,7 @@ const Details = () => {
           <Checkbox defaultChecked>Ship to billing address.</Checkbox>
         </GridItem>
         <GridItem colSpan={2}>
-          <Button size="lg" w="full">
+          <Button variant="primary" size="lg" w="full">
             Place Order
           </Button>
         </GridItem>
