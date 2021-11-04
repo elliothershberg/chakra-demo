346d35aecd0d87bd935ede35f28fe739696ede18
Elliot Hershberg <eahershberg@gmail.com>
2021-11-04 09:41:44 -0700

    Set up basic page

---
+++ src/sections/details.tsx
@@ -0,0 +1,12 @@
+import { VStack, Heading, Text } from "@chakra-ui/react";
+
+const Details = () => {
+  return (
+    <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
+      <Heading size="2xl">Your details</Heading>
+      <Text>If you already have an account, click here to log in.</Text>
+    </VStack>
+  );
+};
+
+export default Details;
